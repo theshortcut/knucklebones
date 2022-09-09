336fe32582af431b74e5a96bfe9a6676d29307df
Clayton Ferris <cferris@gmail.com>
2022-09-09 12:33:22 -0700

    pwa setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,38 @@ import { defineConfig } from 'vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin(), visualizer()],
+  plugins: [
+    react(),
+    vanillaExtractPlugin(),
+    visualizer(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Knucklebones',
+        short_name: 'Knucklebones',
+        description:
+          'Online version of Knucklebones based on the minigame from Cult of the Lamb',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve('./src'),
